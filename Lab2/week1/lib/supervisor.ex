61f69596b68868f0b0b3958a0e1d618d3af1363f
Maxim Bugăescu <maxim.bugaescu.mb@gmail.com>
2023-04-19 02:23:10 +0300

    Lab3 commit

---
+++ Lab2/week1/lib/supervisor.ex
@@ -0,0 +1,18 @@
+defmodule MySupervisor do
+  use Supervisor
+
+  def start_link(init_arg \\ :ok) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  def init(_init_arg) do
+    children = [
+      {Printer, 25},
+      %{id: :reader1,start: {Reader, :start_link, [:r1, "localhost:4000/tweets/1"]}},
+      %{id: :reader2,start: {Reader, :start_link, [:r2, "localhost:4000/tweets/2"]}}
+    ]
+    Supervisor.init(children, strategy: :rest_for_one)
+  end
+end
+
+#   MySupervisor.start_link
